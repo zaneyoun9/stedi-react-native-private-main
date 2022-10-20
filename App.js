229283ac98f3d8074014412ecf427aecf2a45a67
zaneyoun9 <113536094+zaneyoun9@users.noreply.github.com>
2022-10-20 14:58:45 -0600

    Adding

---
+++ App.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  AsyncStorage,
+  TextInput,
+  Button,
+} from "react-native";
+import Navigation from "./components/Navigation";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnboardingScreen from "./screens/OnboardingScreen";
+import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+
+const AppStack = createNativeStackNavigator();
+
+const App = () => {
+  const [isFirstLaunch, setFirstLaunch] = React.useState(true);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+
+  if (isFirstLaunch == true) {
+    return <OnboardingScreen setFirstLaunch={setFirstLaunch} />;
+  } else if (isLoggedIn) {
+    return <Navigation />;
+  } else {
+    return (
+      <view>
+        <TextInput
+          style={StyleSheet.input}
+          placeholderTextColor="#4251f5"
+          placeholder="Phone Number"
+        ></TextInput>
+        <Button
+          title="Send"
+          style={styles.button}
+          onPress={() => {
+            console.log("Button was pressed!");
+          }}
+        />
+      </view>
+    );
+  }
+};
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  margin: {
+    marginTop: 100,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+  },
+});
