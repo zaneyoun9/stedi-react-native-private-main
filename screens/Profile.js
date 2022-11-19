484b662343da8e4bb28bfb95660efcb2a53051c3
zaneyoun9 <113536094+zaneyoun9@users.noreply.github.com>
2022-11-19 13:24:36 -0700

    Added camera feature

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,18 @@
-
-import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView , Share, ScrollView, Button} from 'react-native';
-import { Card, CardTitle, CardContent} from 'react-native-material-cards';
-import BarChart from 'react-native-bar-chart';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  Share,
+  ScrollView,
+  Button,
+} from "react-native";
+import { Card, CardTitle, CardContent } from "react-native-material-cards";
+import BarChart from "react-native-bar-chart";
+import { Camera } from "expo-camera";
 // import Share from 'react-native-share';
-
-
 
 // const data = [
 //   [70, 0],
@@ -21,49 +28,69 @@ import BarChart from 'react-native-bar-chart';
 // const horizontalData = ['S', 'M', 'T', 'W', 'T', 'F','S'];
 
 const Profile = (props) => {
-  const myCustomerShare = async() =>{
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+    };
+    getUserInfo();
+  }, []);
+
+  const myCustomerShare = async () => {
     const shareOptions = {
-      message: 'This is a test'
-    }
-    try{
-      const shareResponse = await Share.share(shareOptions)
+      message: "This is a test",
+    };
+    try {
+      const shareResponse = await Share.share(shareOptions);
       console.log(shareResponse);
-      }
-      catch(error){
-  console.log('Error', error)
-      }
+    } catch (error) {
+      console.log("Error", error);
     }
+  };
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-         <Card style={{backgroundColor:'white', borderRadius: 10, margin:20 ,width: 320, shadowColor: "#000",
-shadowOffset: {
-	width: 0,
-	height: 2,
-},
-shadowOpacity: 0.23,
-shadowRadius: 2.62,
+    <SafeAreaView style={{ flex: 1 }}>
+      <Card
+        style={{
+          backgroundColor: "white",
+          borderRadius: 10,
+          margin: 20,
+          width: 320,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.23,
+          shadowRadius: 2.62,
 
-elevation: 4}}>
-     <CardContent>
-     <Image style={{height: 100, width:100, borderRadius: 75}}
-      source={require('../image/me.jpg')} />
-    <Text style={{marginTop:10,marginBottom:10,fontWeight: 'bold'}}>Sarah Romero</Text>
+          elevation: 4,
+        }}
+      >
+        <CardContent>
+          <Image
+            style={{ height: 100, width: 100, borderRadius: 75 }}
+            source={require("../image/me.jpg")}
+          />
+          <Text style={{ marginTop: 10, marginBottom: 10, fontWeight: "bold" }}>
+            Sarah Romero
+          </Text>
 
-    <Text style={{marginTop:20,marginBottom:2}}>This Week's progress</Text>
-{/* <BarChart barColor='green' data={data} horizontalData={horizontalData} /> */}
-     <View style={{ marginTop: 50 }}>
-      <Button onPress={myCustomerShare} title="Share" />
-    </View>
-    </CardContent>
-    </Card>
- </SafeAreaView>
+          <Text style={{ marginTop: 20, marginBottom: 2 }}>
+            This Week's progress
+          </Text>
+          {/* <BarChart barColor='green' data={data} horizontalData={horizontalData} /> */}
+          <View style={{ marginTop: 50 }}>
+            <Button onPress={myCustomerShare} title="Share" />
+          </View>
+        </CardContent>
+      </Card>
+    </SafeAreaView>
   );
 };
 export default Profile;
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    padding:20
-  }
-})
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
